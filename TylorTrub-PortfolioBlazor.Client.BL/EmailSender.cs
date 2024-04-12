6a5232d61bc48619f5774015a478dd4d743ec839
Tylor <46426868+DontDoThat21@users.noreply.github.com>
2024-04-12 16:51:23 -0400

    Seperated Client from MVC Porfolio project. Now the only shared library is a DTO object. Should refactor this as well, and deliver it via NuGet PckgMngr. Eventually.

---
+++ TylorTrub-PortfolioBlazor.Client.BL/EmailSender.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TylorTrubPortfolio.Client.BL
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            // logic to send an email
+            return Task.CompletedTask;
+        }
+    }
+}
