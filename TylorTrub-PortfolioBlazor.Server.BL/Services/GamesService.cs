b738a01a0df9c946f7231196228cbed11dca67b6
Tylor <46426868+DontDoThat21@users.noreply.github.com>
2024-04-21 16:11:32 -0400

    Improved Motorcycles details, index and store pages. Improved orders index page. Improved home index page. Improved/rewrote projects index page markup.
    Added games service and interface.

---
+++ TylorTrub-PortfolioBlazor.Server.BL/Services/GamesService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TylorTrubPortfolioBlazor.DTO.Models;
+using TylorTrubPortfolioBlazor.Server.BL.Data;
+
+namespace TylorTrubPortfolioBlazor.Server.BL.Services
+{
+    public class GamesService : IGamesService
+    {
+        private readonly PortfolioDBContext dbContext;
+
+        public GamesService(PortfolioDBContext dBContext)
+        {
+            this.dbContext = dBContext;
+        }
+
+        public async Task<List<Game>> FetchGamesAsync()
+        {
+            var games = await dbContext.Games.ToListAsync();
+
+            return games;
+        }
+    }
+}
