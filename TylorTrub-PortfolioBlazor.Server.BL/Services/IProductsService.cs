53d334d43f96bd9a3493fbafda9304c290099b59
Tylor <46426868+DontDoThat21@users.noreply.github.com>
2024-04-21 16:07:53 -0400

    Added products markup and index child component. Added games markup and index child component. Added simon says markup and index child component. Added blazor demo markup and index child component. Added WIP orders markup and index child component.

---
+++ TylorTrub-PortfolioBlazor.Server.BL/Services/IProductsService.cs
@@ -0,0 +1,10 @@
+﻿using TylorTrubPortfolioBlazor.DTO.Models;
+
+namespace TylorTrubPortfolioBlazor.Server.BL.Services
+{
+    public interface IProductsService
+    {
+        Task<List<Product>> FetchProductsAsync();
+        Task<Product> FetchProductsAsync(int productId);
+    }
+}
---
+++ TylorTrub-PortfolioBlazor.Server.BL/Services/ProductsService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TylorTrubPortfolioBlazor.DTO.Models;
+using TylorTrubPortfolioBlazor.Server.BL.Data;
+
+namespace TylorTrubPortfolioBlazor.Server.BL.Services
+{
+    public class ProductsService : IProductsService
+    {
+        private readonly PortfolioDBContext dbContext;
+
+        public ProductsService(PortfolioDBContext dBContext)
+        {
+            this.dbContext = dBContext;
+        }
+
+        public async Task<List<Product>> FetchProductsAsync()
+        {
+            var products = await dbContext.Products.ToListAsync();
+
+            return products;
+        }
+        
+        public async Task<Product> FetchProductsAsync(int productId)
+        {
+            //Motorcycle motorcycle = (Motorcycle)await dbContext.FindAsync(motorcycleId);
+            //return motorcycle;
+
+            return await dbContext.Products.FindAsync(productId);
+        }
+    }
+}
