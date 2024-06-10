fbe6c8879ebf86984d501a64ee373c8cb16bf84b
Tylor <46426868+DontDoThat21@users.noreply.github.com>
2024-06-10 17:51:15 -0400

    Adding new .NET 8 auth.

--- TylorTrub-PortfolioBlazor/Program.cs
+++ TylorTrub-PortfolioBlazor/Program.cs
@@ -15,36 +15,38 @@ using MudExtensions.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
 using Fluxor.Blazor.Web.ReduxDevTools;
+using TylorTrubPortfolio.Server.BL.Repository;
+using System.Security.Claims;
 
 namespace TylorTrubPortfolioBlazor
 {
-    public class Program()
+    public class Program
     {
         public static IConfiguration Configuration { get; set; }
 
         public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            
+
             // Add services to the container.
             builder.Services.AddRazorPages();
-            builder.Services.AddServerSideBlazor();           
+            builder.Services.AddServerSideBlazor();
 
-            //builder.Services.AddDbContext<PortfolioDBContext>(options =>
-            //  options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+            builder.Services.AddAuthentication().AddBearerToken(IdentityConstants.BearerScheme);
+            builder.Services.AddAuthorizationBuilder();
 
-            // new identity db context
+            // Add DbContext
             builder.Services.AddDbContext<PortfolioDBContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-            builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<PortfolioDBContext>().AddDefaultTokenProviders();
-            builder.Services.ConfigureApplicationCookie(options =>
-            {
-                options.LoginPath = $"/Identity/Account/Login";
-                options.LogoutPath = $"/Identity/Account/Logout";
-                options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
-            });
+            builder.Services.AddIdentityCore<IdentityUser>()
+                .AddEntityFrameworkStores<PortfolioDBContext>()
+                .AddApiEndpoints();
 
+            // Add Controllers with Views
+            builder.Services.AddControllersWithViews();
+
+            // Add other services
             builder.Services.AddScoped<IPortfolioImagesService, PortfolioImagesService>();
             builder.Services.AddScoped<IProjectsService, ProjectsService>();
             builder.Services.AddScoped<IMotorcycleVideosService, MotorcycleVideosService>();
@@ -57,48 +59,60 @@ namespace TylorTrubPortfolioBlazor
             builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
             builder.Services.AddScoped<TylorTrubPortfolio.Client.BL.IEmailSender, TylorTrubPortfolio.Client.BL.EmailSender>();
 
+            // Add Blazor and MudBlazor services
             builder.Services.AddRazorComponents().AddInteractiveServerComponents();
             builder.Services.AddCascadingAuthenticationState();
             builder.Services.AddMudBlazorDialog()
                 .AddMudServices()
                 .AddHotKeys2();
 
+            // Add Fluxor
             builder.Services.AddFluxor(options =>
             {
                 options.ScanAssemblies(Assembly.GetExecutingAssembly());
                 options.UseReduxDevTools();
             });
 
+            // Add HttpContextAccessor
             builder.Services.AddHttpContextAccessor();
             builder.Services.AddMudExtensions();
 
+            // Configure forwarded headers
             builder.Services.Configure<ForwardedHeadersOptions>(options =>
             {
-                options.ForwardedHeaders =
-                    ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+                options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
             });
 
             var app = builder.Build();
-            
+
+            app.MapGet("/", (ClaimsPrincipal user) => $"Hello {user.Identity!.Name}")
+                .RequireAuthorization();
+
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
-
             app.UseStaticFiles();
-            
+
             app.UseRouting();
             
+            app.UseAuthentication();
+            app.UseAuthorization();
+
             app.MapBlazorHub();
             app.MapFallbackToPage("/_Host");
-            
+
+            app.MapRazorPages();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
             app.Run();
         }
     }
 }
-
